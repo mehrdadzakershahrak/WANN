8702e1920db9ab60aaac8c46cb2aacb0e1d2e912
Blake Harrison <blcharri@iu.edu>
2020-09-15 15:57:24 -0700

    cp

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -3,10 +3,10 @@ from rlkit.torch.sac.sac import SACTrainer as SoftActorCritic_rlkit
 
 
 class Agent(object):
-    def __init__(self, eval_env, expl_env, mem, train_params, alg_params, nets):
+    def __init__(self, env, eval_env, mem, train_params, alg_params, nets):
+        self._env = eval_env
         self._eval_env = eval_env
-        self._expl_env = expl_env
         self._mem = mem
-        self._nets = nets
         self._train_params = train_params
         self._alg_params = alg_params
+        self._nets = nets
